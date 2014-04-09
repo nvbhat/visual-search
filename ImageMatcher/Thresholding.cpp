12e148609daa9917dc7a81174418825c496da038
Sai Susarla <sai.susarla@gmail.com>
2014-04-09 23:07:49 +0530

    Initial commit of template image matching tool

---
+++ ImageMatcher/Thresholding.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <limits>
+ 
+ using namespace cv;
+ using namespace std;
+  Mat inImage = cv::imread(".jpg");
+   
+       imshow("Original", inImage);
+
+
+
+
