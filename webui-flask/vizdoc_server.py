66a55e613742a3f72e1c6c6d32c1acfb1593ae01
Narayana Bhat <narayanavbhat@gmail.com>
2015-04-03 22:00:51 +0530

    added mongodb connection and getbooks API

--- webui-flask/vizdoc_server.py
+++ webui-flask/vizdoc_server.py
@@ -1,5 +1,5 @@
 import os
-import requests
+from flask import request
 from os import path
 from flask.ext.pymongo import PyMongo
 from flask import *
@@ -8,12 +8,20 @@ import json,time
 from werkzeug import secure_filename
 #import datetime
 from vizdoc_config import *
+#import vizdoc_user
+#import vizdoc_anno
+
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','zip'])
+
 app = Flask(__name__)
+#mongo = PyMongo(app, config_prefix='MONGO2')
+#mongo = PyMongo(app)
 # connect to another MongoDB database on the same host
 app.config['MONGO2_DBNAME'] = 'vizdoc_db'
 mongo = PyMongo(app, config_prefix='MONGO2')
+
+print "Before registering APIs"
  
 def allowed_file(filename):
     return '.' in filename and \
@@ -66,13 +74,6 @@ def upload_book():
     #return redirect(url_for('dir_listing', dirname=dirname))
     return render_template('uploadbook.html',filenames=filenames)
 
-@app.route('/getBook/<bookid>', methods=['GET','POST'])
-def getBook(bookid):
-        print "processing " + bookid
-        fname = PUBSTORE+"/books/"+bookid+".json"
-        print fname
-	return send_from_directory(PUBSTORE+"/books",bookid+".json")
-
 @app.route('/uploadpage', methods=['GET', 'POST'])
 def upload_file():
 #    if request.method == 'POST':
@@ -110,24 +111,24 @@ def uploaded_files(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
-@app.route('/<path:req_path>')
-def dir_listing(req_path):
-    BASE_DIR = ROOTDIR  #'/home/vtbhat/vsearch_db'
-
-    # Joining the base and the requested path
-    abs_path = os.path.join(BASE_DIR, req_path)
-
-    # Return 404 if path doesn't exist
-    if not os.path.exists(abs_path):
-        return abort(404)
-  	
-    # Check if path is a file and serve
-    if os.path.isfile(abs_path):
-        return send_file(abs_path)
-
-    # Show directory contents
-    files = os.listdir(abs_path)
-    return render_template('files.html', files=files)
+#@app.route('/<path:req_path>')
+#def dir_listing(req_path):
+#    BASE_DIR = ROOTDIR  #'/home/vtbhat/vsearch_db'
+#
+#    # Joining the base and the requested path
+#    abs_path = os.path.join(BASE_DIR, req_path)
+#
+#    # Return 404 if path doesn't exist
+#    if not os.path.exists(abs_path):
+#        return abort(404)
+#  	
+#    # Check if path is a file and serve
+#    if os.path.isfile(abs_path):
+#        return send_file(abs_path)
+#
+#    # Show directory contents
+#    files = os.listdir(abs_path)
+#    return render_template('files.html', files=files)
 
 app.secret_key = 'milan322'
 
@@ -177,11 +178,55 @@ def login():
            return redirect(url_for('hello'))
     return render_template('login.html',error=error)
 
+@app.route('/getbooklist')
+def filelist():
+    arr=[]
+    for root, dirs, files in os.walk(ROOTDIR):
+    #print('Found directory: %s' % root)
+        for file in files:
+            if file.endswith(".json"):
+                 abspath = os.path.join(root,file) 
+                 newfile = str.replace(abspath, ROOTDIR+"/", "");
+                 print (newfile)
+                 sendpath=newfile
+                 data=sendpath[1]
+
+            # print('\t%s' % file)
+             #converting RAW data of python array element into json files.
+                 arr.append(str(newfile))
+#    return '{"books":'+arr+'}'
+    print (arr)
+    return json.dumps(arr)
+
+
+"""
+    jsonfile = "filelist.json"
+    d={
+        "paths": ""
+      }
+    d['paths']=arr
+    json.dumps(d)
+    print (d)
+    with open(ROOTDIR + "/" + jsonfile, 'w') as outfile:
+        json.dump(d, outfile, sort_keys = True, indent = 4,ensure_ascii=False)
+    # print (s'Successfully saved!')
+#    return (d)
+    return send_from_directory(ROOTDIR,jsonfile)
+"""
+
 @app.route('/static/<path:filepath>')
 def getFile(filepath):
+    print "Entered getFile"
     abspath = ROOTDIR + "/" + filepath
     print abspath
     return send_file(abspath)
+
+@app.route('/getbooks/<bookid>', methods=['GET','POST'])
+def getBook(bookid):
+        print "processing " + bookid
+        fname = PUBSTORE+"/books/"+bookid+".json"
+        print fname
+        return send_from_directory(PUBSTORE+"/books",bookid+".json")
 
 @app.route('/saveanno',methods=['GET','POST'])
 def saveanno():
@@ -193,17 +238,8 @@ def saveanno():
                     'text': request.get('text'), 'comment': request.get('comment')},
         safe=True, upsert=True)
 
-@app.route('/getanno',methods=['GET','POST'])
-def getanno():
-    impath = request.args.get('imagepath',type=str)
-    coord = request.args.get('coord',type=str)
-    anno = mongo.db.annotations.find_one(
-        {'file': impath, 'coord': coord})
-    return anno 
-#        {'ratings': request.get('ratings'), 'name': request.get('name'), 
-#                    'text': request.get('text'), 'comment': request.get('comment')},
-#        safe=True, upsert=True)
-
+#def getcontent():
+     
 """      
 @app.route('/<path:pagepath>')
 def show_page(pagepath):
@@ -241,31 +277,42 @@ def new_form(error):
     if not pagepath.startswith('uploads'):
         return render_template('form.html', page=None, pagepath=pagepath)
 """
-@app.route('/getPage/<page>', methods=['GET','POST'])
-def getAnnotation(page):
+
+@app.route('/getpage/<page>', methods=['GET','POST'])
+def getpage(page):
     pname = PUBSTORE+"/books/"+page+".json"
     print pname
-#    image = request.args.get('impath',type=str)
-    components = pname.split('/')
-#    del components[-1]
-    extract = components[-1].split('.')
-    basename = extract[0]
-#    extension = "_annotation.json"
-#    finaljson =  basename + extension
+    #image = request.args.get('impath',type=str)
+    #components = pname.split('/')
+    #del components[-1]
+    #extract = components[-1].split('.')
+    #basename = extract[0]
+    #extension = "_annotation.json"
+    #finaljson =  basename + extension
 #    jsonpath = ROOTDIR + "/"+ '/'.join(components) + "/segments/" + basename + extension
-#    print jsonpath	
-#    if path.exists(jsonpath):
-    with open(pname) as p:
-#            bookjson=json.load(f)
-        print "something"
+    print jsonpath	
+    if path.exists(jsonpath):
+        with open(pname) as p:
+            bookjson=json.load(f)
+        print "all directory files"
         return send_from_directory(PUBSTORE+"/books",basename+".json")
 
-#@app.route('/coords',methods=['GET','POST'])
-#def coords():
-#    coords = request.args.get('pass3',type=str)
-#    print coords
-#    return render_template('form.html',coords=coords)
+@app.route('/getanno/<coord>',methods=['GET','POST'])
+@app.route('/getanno/<coord>/<imagepath>',methods=['GET','POST'])
+def getanno(coord,imagepath=None):
+#    impath = request.args.get('imagepath',type=str)
+#    coord = request.args.get('coord',type=str)
+    print "getting mongodb contents"
+    anno = mongo.db.annotations.find_one_or_404({'coord':coord,'imagepath':imagepath})
+    print "successful"
+    print anno
+#        {'ratings': request.get('ratings'), 'name': request.get('name'), 
+#                    'text': request.get('text'), 'comment': request.get('comment')},
+#        safe=True, upsert=True)
 
+
+
+"""
 @app.route('/segment',methods=['GET','POST'])
 def segment():
     from imagestojson import *
@@ -282,18 +329,22 @@ def segment():
     if not path.exists(jsonpath):
         rv1 = os.system("python imagestojson.py static/visual-search/images/"+dividing[-1]) 
         if rv1 == 0: 
-            rv2 = os.system("python mainimgsegmenter.py -j static/visual-search/indexedbooks/"+jsonfile+" -b "+finaljson) 
+            rv2 = os.system("python mainimgsegmenter.py -j static/"+jsonfile+" -b "+finaljson) 
     else:
         print "file already exists"
     return jsonify(result=jsonpath) 
 
+"""
+
+
+print "After registering APIs"
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
-#    app.run(debug=True)
     app.run(host = '0.0.0.0',debug=True)  
 
-#def getAnnotations
+#def getAnnotations complete
 
 #def delAnnotation
 
